60faa712424731c2d5c06586e4886172c5782f0c
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2021-11-29 23:14:19 +0900

    feat: 전략패턴 익명클래스 람다

--- src/test/java/co/wordbe/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/co/wordbe/advanced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package co.wordbe.advanced.trace.strategy;
 
 import co.wordbe.advanced.trace.strategy.code.strategy.ContextV1;
+import co.wordbe.advanced.trace.strategy.code.strategy.Strategy;
 import co.wordbe.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -45,5 +46,57 @@ public class ContextV1Test {
         StrategyLogic1 strategyLogic2 = new StrategyLogic1();
         ContextV1 contextV12 = new ContextV1(strategyLogic2);
         contextV12.excute();
+    }
+
+    @Test
+    void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스로직1 실행;");
+            }
+        };
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        log.info("strategyLogic1={}", strategyLogic1.getClass());
+        contextV1.excute();
+
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스로직1 실행;");
+            }
+        };
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        log.info("strategyLogic2={}", strategyLogic2.getClass());
+        contextV2.excute();
+    }
+
+    @Test
+    void strategyV3() {
+        ContextV1 contextV1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스로직1 실행;");
+            }
+        });
+        contextV1.excute();
+
+        ContextV1 contextV2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스로직1 실행;");
+            }
+        });
+        contextV2.excute();
+    }
+
+    @Test
+    void strategyV4() {
+        // lambda 를 사용하려면 인터페이스에 메소드가 1개만 있어야 한다.
+        ContextV1 contextV1 = new ContextV1(() -> log.info("비즈니스로직1 실행;"));
+        contextV1.excute();
+
+        ContextV1 contextV2 = new ContextV1(() -> log. info("비즈니스로직1 실행;"));
+        contextV2.excute();
     }
 }
