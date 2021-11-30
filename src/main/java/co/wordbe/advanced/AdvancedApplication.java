15d6f0472ffced1b20967362deb66aa7348aecd0
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2021-11-30 23:49:00 +0900

    feat: 인터페이스 없이 만들기 v2

--- src/main/java/co/wordbe/advanced/AdvancedApplication.java
+++ src/main/java/co/wordbe/advanced/AdvancedApplication.java
@@ -1,11 +1,13 @@
 package co.wordbe.advanced;
 
 import co.wordbe.advanced.config.AppV1Config;
+import co.wordbe.advanced.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
+//@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "co.wordbe.advanced.app.proxy")
 public class AdvancedApplication {
 
