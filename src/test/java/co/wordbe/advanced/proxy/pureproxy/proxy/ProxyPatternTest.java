c1bbf3348615e0f8f03e064f919e47a56f84e8c1
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2021-12-01 01:53:10 +0900

    feat: 데코레이터 패턴 테스트

--- src/test/java/co/wordbe/advanced/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/co/wordbe/advanced/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package co.wordbe.advanced.proxy.pureproxy.proxy;
 
+import co.wordbe.advanced.proxy.pureproxy.proxy.code.CacheProxy;
 import co.wordbe.advanced.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import co.wordbe.advanced.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,16 @@ public class ProxyPatternTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
