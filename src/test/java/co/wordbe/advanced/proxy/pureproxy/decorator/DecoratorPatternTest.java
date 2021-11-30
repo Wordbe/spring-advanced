c529e8500543d3bda8863cf9c34473cac8ed18c3
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2021-12-01 02:00:17 +0900

    feat: 메시지 데코레이터 추가

--- src/test/java/co/wordbe/advanced/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/co/wordbe/advanced/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,6 +1,8 @@
 package co.wordbe.advanced.proxy.pureproxy.decorator;
 
+import co.wordbe.advanced.proxy.pureproxy.decorator.code.Component;
 import co.wordbe.advanced.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import co.wordbe.advanced.proxy.pureproxy.decorator.code.MessageDecorator;
 import co.wordbe.advanced.proxy.pureproxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,14 @@ public class DecoratorPatternTest {
     void noDecorator() {
         RealComponent realComponent = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
         client.execute();
     }
 }
