a57b0a4f99b94c87a69a339e54e55b819601686d
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2021-12-05 03:04:07 +0900

    feat: V2 에 ProxyFactory 적용

--- src/main/java/co/wordbe/advanced/AdvancedApplication.java
+++ src/main/java/co/wordbe/advanced/AdvancedApplication.java
@@ -4,6 +4,7 @@ import co.wordbe.advanced.config.v1_proxy.ConcreteProxyConfig;
 import co.wordbe.advanced.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import co.wordbe.advanced.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import co.wordbe.advanced.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import co.wordbe.advanced.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import co.wordbe.advanced.trace.logtrace.LogTrace;
 import co.wordbe.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -17,7 +18,8 @@ import org.springframework.context.annotation.Import;
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
-@Import(ProxyFactoryConfigV1.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "co.wordbe.advanced.app.proxy")
 public class AdvancedApplication {
 
