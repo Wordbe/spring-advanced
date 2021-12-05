c2dc0e2aeada48a7d1c8451461f15d7803c8f6da
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2021-12-05 21:45:17 +0900

    feat: 자동프록시 생성기로 어드바이저 등록

--- src/main/java/co/wordbe/advanced/config/v5_autoproxy/AutoProxyConfig.java
+++ src/main/java/co/wordbe/advanced/config/v5_autoproxy/AutoProxyConfig.java
@@ -5,6 +5,7 @@ import co.wordbe.advanced.config.AppV2Config;
 import co.wordbe.advanced.config.v3_proxyfactory.advice.LogTraceAdvice;
 import co.wordbe.advanced.trace.logtrace.LogTrace;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +16,31 @@ import org.springframework.context.annotation.Import;
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-    @Bean
+//    @Bean // 등록 안함
     public Advisor advisor1(LogTrace logTrace) {
         // pointcut
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("order*", "orderItem*", "save*");
+        // advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+//    @Bean
+    public Advisor advisor2(LogTrace logTrace) {
+        // pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* co.wordbe.advanced.app.proxy..*(..))");
+        // advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    @Bean
+    public Advisor advisor3(LogTrace logTrace) {
+        // pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* co.wordbe.advanced.app.proxy..*(..)) && !execution(* co.wordbe.advanced.app.proxy..noLog(..))");
         // advice
         LogTraceAdvice advice = new LogTraceAdvice(logTrace);
         return new DefaultPointcutAdvisor(pointcut, advice);
